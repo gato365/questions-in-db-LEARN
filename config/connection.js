341dbae0920468aa7521ec7732727ae823a9f8af
gato365 <immanuelwilliams@gato365.com>
2023-01-27 08:00:24 -0800

    Added more libs and changed connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,22 @@
-// const mysql = require("mysql2");
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-// const connection = mysql.createConnection({
-//     uri: process.env.CLEARDB_DATABASE_URL,
+// let sequelize;
 
-// })
+// **** This is for Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_DB,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
-module.exports = connection;
+module.exports = sequelize;
