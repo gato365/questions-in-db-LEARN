c36eb2d85d299a0fcb8957514fe86b761d6d7730
gato365 <immanuelwilliams@gato365.com>
2023-01-27 08:10:58 -0800

    Created Notes js model

--- models/Notes.js
+++ models/Notes.js
@@ -1,1 +1,38 @@
 // Notes Model using Sequelize
+const { Model, DataTypes } = require('sequelize');
+const notes = require('../routes/notes');
+const sequelize = require('./config/connection');
+
+
+Notes.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'notes'
+    }
+);
+
+module.exports = Notes;
+
+
+
+
+
